e6cff35ce55568f08c768ad2b8bc4e19d71317c8
Lateef Ojulari <lateefojulari@gmail.com>
2018-12-11 18:09:34 +0100

    Update copyright

--- unify-core/src/main/java/com/tcdng/unify/core/database/sql/dialect/JavaDbDialect.java
+++ unify-core/src/main/java/com/tcdng/unify/core/database/sql/dialect/JavaDbDialect.java
@@ -1,5 +1,5 @@
 /*
-<? extends Entity> * Copyright 2014 The Code Department
+<? extends Entity> * Copyright 2018 The Code Department
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
