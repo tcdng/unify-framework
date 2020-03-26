648a956b771affc260d0d60a8be544b60a02c99f
Lateef Ojulari <lateef.ojulari@tcdng.com>
2020-03-26 23:55:11 +0100

    Add more runtime java class manager tests

--- unify-core/src/main/java/com/tcdng/unify/core/runtime/RuntimeJavaClassManagerImpl.java
+++ unify-core/src/main/java/com/tcdng/unify/core/runtime/RuntimeJavaClassManagerImpl.java
@@ -136,7 +136,7 @@ public class RuntimeJavaClassManagerImpl extends AbstractRuntimeJavaClassManager
     @Override
     public Class<?> getSavedJavaClass(String groupName, String className) throws UnifyException {
         try {
-            return Class.forName(groupName, true, groupSavedJavaClassLoaders.get(groupName));
+            return Class.forName(className, true, groupSavedJavaClassLoaders.get(groupName));
         } catch (ClassNotFoundException e) {
             throwOperationErrorException(e);
         }
@@ -146,56 +146,48 @@ public class RuntimeJavaClassManagerImpl extends AbstractRuntimeJavaClassManager
 
     @Override
     public long getSavedJavaClassVersion(String groupName, String className) throws UnifyException {
-        SingleVersionLargeObjectService svlos =
-                (SingleVersionLargeObjectService) getComponent(
-                        ApplicationComponents.APPLICATION_SINGLEVERSIONLOBSERVICE);
+        SingleVersionLargeObjectService svlos = (SingleVersionLargeObjectService) getComponent(
+                ApplicationComponents.APPLICATION_SINGLEVERSIONLOBSERVICE);
         return svlos.getBlobVersion(RUNTIMECLASS_APPLICATION, groupName, className);
+    }
+
+    @Override
+    public void clearCachedSaveJavaClasses(String groupName) throws UnifyException {
+        groupSavedJavaClassLoaders.remove(groupName);
     }
 
     private class GroupSavedJavaClassLoader extends ClassLoader {
 
         private String groupName;
-        
-        
+
         public GroupSavedJavaClassLoader(String groupName) {
             this.groupName = groupName;
         }
 
-
         @Override
-        protected Class<?> loadClass(String className, boolean resolve) throws ClassNotFoundException {
-            // We are not checking parent so we implement loadClass()
-            Class<?> clazz = findLoadedClass(className);
-            if (clazz != null) {
-                return clazz;
-            }
-            
+        protected Class<?> findClass(String className) throws ClassNotFoundException {
             SecurityManager sm = System.getSecurityManager();
-            int pIndex = className.lastIndexOf('.');
-            if (pIndex >= 0) {
-                sm.checkPackageDefinition(className.substring(0, pIndex));
+            if (sm != null) {
+                int pIndex = className.lastIndexOf('.');
+                if (pIndex >= 0) {
+                    sm.checkPackageDefinition(className.substring(0, pIndex));
+                }
             }
-            
+
             byte[] byteCode = null;
             try {
-                SingleVersionLargeObjectService svlos =
-                        (SingleVersionLargeObjectService) getComponent(
-                                ApplicationComponents.APPLICATION_SINGLEVERSIONLOBSERVICE);
+                SingleVersionLargeObjectService svlos = (SingleVersionLargeObjectService) getComponent(
+                        ApplicationComponents.APPLICATION_SINGLEVERSIONLOBSERVICE);
                 byteCode = svlos.retrieveBlob(RUNTIMECLASS_APPLICATION, groupName, className);
             } catch (UnifyException e) {
                 throw new ClassNotFoundException(className, e);
             }
-            
+
             if (byteCode == null) {
                 throw new ClassNotFoundException(className);
             }
-            
-            clazz = defineClass(className, byteCode, 0, byteCode.length);
-            if (resolve) {
-                resolveClass(clazz);
-            }
-            
-            return clazz;
+
+            return defineClass(className, byteCode, 0, byteCode.length);
         }
 
     }
@@ -214,30 +206,25 @@ public class RuntimeJavaClassManagerImpl extends AbstractRuntimeJavaClassManager
             throws UnifyException {
         final String className = srcDetails.getClassName();
         final long version = srcDetails.getVersion();
-        SingleVersionLargeObjectService svlos =
-                (SingleVersionLargeObjectService) getComponent(
-                        ApplicationComponents.APPLICATION_SINGLEVERSIONLOBSERVICE);
+        SingleVersionLargeObjectService svlos = (SingleVersionLargeObjectService) getComponent(
+                ApplicationComponents.APPLICATION_SINGLEVERSIONLOBSERVICE);
         if (svlos.getBlobVersion(RUNTIMECLASS_APPLICATION, groupName, className) < version) {
             synchronized (LockUtils.getStringLockObject(StringUtils.dotify(RUNTIMECLASS_APPLICATION, groupName))) {
-                if (svlos.getBlobVersion(RUNTIMECLASS_APPLICATION, groupName,
-                        className) < version) {
-                    synchronized (LockUtils
-                            .getStringLockObject(StringUtils.dotify(RUNTIMECLASS_APPLICATION, groupName))) {
-                        try {
-                            SimpleCompiler compiler = new SimpleCompiler();
-                            compiler.cook(reader);
-                            byte[] byteCode = compiler.getBytecodes().get(className);
-                            boolean result = svlos.storeBlob(RUNTIMECLASS_APPLICATION,
-                                    groupName, className, byteCode, version);
-                            if (result) {
-                                // Invalidate group class loader
-                                groupSavedJavaClassLoaders.remove(groupName);
-                            }
-
-                            return result;
-                        } catch (Exception e) {
-                            throw new UnifyException(e, UnifyCoreErrorConstants.COMPILER_CLASSLOAD_ERROR);
+                if (svlos.getBlobVersion(RUNTIMECLASS_APPLICATION, groupName, className) < version) {
+                    try {
+                        SimpleCompiler compiler = new SimpleCompiler();
+                        compiler.cook(reader);
+                        byte[] byteCode = compiler.getBytecodes().get(className);
+                        boolean result =
+                                svlos.storeBlob(RUNTIMECLASS_APPLICATION, groupName, className, byteCode, version);
+                        if (result) {
+                            // Invalidate group class loader
+                            clearCachedSaveJavaClasses(groupName);
                         }
+
+                        return result;
+                    } catch (Exception e) {
+                        throw new UnifyException(e, UnifyCoreErrorConstants.COMPILER_CLASSLOAD_ERROR);
                     }
                 }
             }
