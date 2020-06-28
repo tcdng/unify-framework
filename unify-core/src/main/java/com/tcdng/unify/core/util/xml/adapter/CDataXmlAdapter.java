4fc959871ce968c79f919c7b5a421bbdf8ca6dc0
Lateef Ojulari <lateef.ojulari@tcdng.com>
2020-06-28 19:09:52 +0100

    Implement CDATA XML adapter

---
+++ unify-core/src/main/java/com/tcdng/unify/core/util/xml/adapter/CDataXmlAdapter.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2018-2020 The Code Department.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.tcdng.unify.core.util.xml.adapter;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ * CData XML adapter.
+ * 
+ * @author Lateef Ojulari
+ * @since 1.0
+ */
+public class CDataXmlAdapter extends XmlAdapter<String, String> {
+
+	private static final String CDATA_START = "<![CDATA[";
+
+	private static final String CDATA_END = "]]>";
+
+	private static final String CDATA_BLANK = CDATA_START + CDATA_END;
+
+	@Override
+	public String marshal(String val) throws Exception {
+		if (val != null) {
+			return CDATA_START + val + CDATA_END;
+		}
+
+		return CDATA_BLANK;
+	}
+
+	@Override
+	public String unmarshal(String val) throws Exception {
+		if (val != null && val.startsWith(CDATA_START) && val.endsWith(CDATA_END)) {
+			return val.substring(CDATA_START.length(), val.length() - CDATA_END.length());
+
+		}
+
+		return val;
+	}
+
+}
---
+++ unify-core/src/test/java/com/tcdng/unify/core/util/xml/adapter/CDataXmlAdapterTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018-2020 The Code Department.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.tcdng.unify.core.util.xml.adapter;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+/**
+ * CData XML adapter tests.
+ * 
+ * @author Lateef Ojulari
+ * @since 1.0
+ */
+public class CDataXmlAdapterTest {
+
+	@Test
+	public void testMarshallNull() throws Exception {
+		CDataXmlAdapter adapter = new CDataXmlAdapter();
+		assertEquals("<![CDATA[]]>", adapter.marshal(null));
+	}
+
+	@Test
+	public void testMarshall() throws Exception {
+		CDataXmlAdapter adapter = new CDataXmlAdapter();
+		assertEquals("<![CDATA[Hello World!]]>", adapter.marshal("Hello World!"));
+		assertEquals("<![CDATA[<span>Hello World!</span>]]>", adapter.marshal("<span>Hello World!</span>"));
+	}
+
+	@Test
+	public void testUnmarshallNull() throws Exception {
+		CDataXmlAdapter adapter = new CDataXmlAdapter();
+		assertNull(adapter.unmarshal(null));
+	}
+
+	@Test
+	public void testUnmarshallPlain() throws Exception {
+		CDataXmlAdapter adapter = new CDataXmlAdapter();
+		assertEquals("Hello World!", adapter.unmarshal("Hello World!"));
+	}
+
+	@Test
+	public void testUnmarshallCData() throws Exception {
+		CDataXmlAdapter adapter = new CDataXmlAdapter();
+		assertEquals("Hello World!", adapter.unmarshal("<![CDATA[Hello World!]]>"));
+		assertEquals("<span>Hello World!</span>", adapter.unmarshal("<![CDATA[<span>Hello World!</span>]]>"));
+	}
+}
