35cdcec1958d0e824ffe6629e04004536f22bc08
Lateef Ojulari <lateef.ojulari@tcdng.com>
2022-09-21 18:00:40 +0100

    Fix default extensions

--- unify-web-ui/src/main/java/com/tcdng/unify/web/ui/widget/control/FileUploadView.java
+++ unify-web-ui/src/main/java/com/tcdng/unify/web/ui/widget/control/FileUploadView.java
@@ -39,165 +39,167 @@ import com.tcdng.unify.web.ui.widget.data.FileAttachmentInfo;
  */
 @Component("ui-fileuploadview")
 @UplAttributes({ @UplAttribute(name = "type", type = FileAttachmentType.class, defaultVal = "wildcard"),
-    	@UplAttribute(name = "filenameBinding", type = String.class),
-        @UplAttribute(name = "handler", type = String.class, mandatory = false),
-        @UplAttribute(name = "category", type = String.class),
-        @UplAttribute(name = "parentCategory", type = String.class),
-        @UplAttribute(name = "parentFieldName", type = String.class),
-        @UplAttribute(name = "viewPath", type = String.class) })
+		@UplAttribute(name = "filenameBinding", type = String.class),
+		@UplAttribute(name = "handler", type = String.class, mandatory = false),
+		@UplAttribute(name = "category", type = String.class),
+		@UplAttribute(name = "parentCategory", type = String.class),
+		@UplAttribute(name = "parentFieldName", type = String.class),
+		@UplAttribute(name = "viewPath", type = String.class) })
 public class FileUploadView extends AbstractMultiControl {
 
-    private FileUpload fileCtrl;
+	private FileUpload fileCtrl;
 
-    private Control attachCtrl;
+	private Control attachCtrl;
 
-    private Control viewCtrl;
+	private Control viewCtrl;
 
-    private Control removeCtrl;
+	private Control removeCtrl;
 
-    private FileUploadViewHandler handler;
-    
-    @Override
-    public void populate(DataTransferBlock transferBlock) throws UnifyException {
-        if (transferBlock != null) {
-            DataTransferBlock nextBlock = transferBlock.getChildBlock();
-            Object value = nextBlock.getValue();
-            UploadedFile uploadedFile = ((UploadedFile[]) value)[0];
-            if (handler == null) {
-            	setFilename(uploadedFile.getFilename());
-                setValue(uploadedFile.getData());
-            } else {
-                String category = getUplAttribute(String.class, "category");
-                FileAttachmentType type = getUplAttribute(FileAttachmentType.class, "type");
-                Object uploadId = handler.save(getUploadId(), category, type, uploadedFile.getFilename(),
-                        uploadedFile.getData());
-                setUploadId(uploadId);
-            }
-        }
-    }
+	private FileUploadViewHandler handler;
 
-    @Action
-    public void view() throws UnifyException {
-    	if (handler == null) {
-    		byte[] data =  getValue(byte[].class);
-    		if (data != null) {
-    			final FileAttachmentType type = getType();
+	@Override
+	public void populate(DataTransferBlock transferBlock) throws UnifyException {
+		if (transferBlock != null) {
+			DataTransferBlock nextBlock = transferBlock.getChildBlock();
+			Object value = nextBlock.getValue();
+			UploadedFile uploadedFile = ((UploadedFile[]) value)[0];
+			if (handler == null) {
+				setFilename(uploadedFile.getFilename());
+				setValue(uploadedFile.getData());
+			} else {
+				String category = getUplAttribute(String.class, "category");
+				FileAttachmentType type = getUplAttribute(FileAttachmentType.class, "type");
+				Object uploadId = handler.save(getUploadId(), category, type, uploadedFile.getFilename(),
+						uploadedFile.getData());
+				setUploadId(uploadId);
+			}
+		}
+	}
+
+	@Action
+	public void view() throws UnifyException {
+		if (handler == null) {
+			byte[] data = getValue(byte[].class);
+			if (data != null) {
+				final FileAttachmentType type = getType();
 				FileAttachmentInfo fileAttachmentInfo = new FileAttachmentInfo(type);
-				String filename = getFilename();
-				fileAttachmentInfo.setFilename(!StringUtils.isBlank(filename) ? filename : type.appendDefaultExtension("file"));
+				String filename = getFilename(type);
+				fileAttachmentInfo.setFilename(filename);
 				fileAttachmentInfo.setAttachment(data);
-                setRequestAttribute(UnifyWebRequestAttributeConstants.FILEATTACHMENTS_INFO, fileAttachmentInfo);
-                setCommandResultMapping(ResultMappingConstants.SHOW_ATTACHMENT);
-    		}
-    	} else {
-            // Setup view
-            Object uploadId = getUploadId();
-            if (uploadId != null) {
-                String category = getUplAttribute(String.class, "category");
-                FileAttachmentType type = getUplAttribute(FileAttachmentType.class, "type");
-                FileAttachmentInfo fileAttachmentInfo = handler.retrive(uploadId, category, type);
-                setRequestAttribute(UnifyWebRequestAttributeConstants.FILEATTACHMENTS_INFO, fileAttachmentInfo);
-                setCommandResultMapping(ResultMappingConstants.SHOW_ATTACHMENT);
-            }
-    	}
-    }
+				setRequestAttribute(UnifyWebRequestAttributeConstants.FILEATTACHMENTS_INFO, fileAttachmentInfo);
+				setCommandResultMapping(ResultMappingConstants.SHOW_ATTACHMENT);
+			}
+		} else {
+			// Setup view
+			Object uploadId = getUploadId();
+			if (uploadId != null) {
+				String category = getUplAttribute(String.class, "category");
+				FileAttachmentType type = getUplAttribute(FileAttachmentType.class, "type");
+				FileAttachmentInfo fileAttachmentInfo = handler.retrive(uploadId, category, type);
+				setRequestAttribute(UnifyWebRequestAttributeConstants.FILEATTACHMENTS_INFO, fileAttachmentInfo);
+				setCommandResultMapping(ResultMappingConstants.SHOW_ATTACHMENT);
+			}
+		}
+	}
 
-    @Action
-    public void detach() throws UnifyException {
-    	if (handler == null) {
-        	setFilename(null);
-    		setValue(null);
-    	} else {
-            // Detach
-            Object uploadId = getUploadId();
-            Object parentId = null;
-            Object valueObject = getValueStore().getValueObject();
-            if (valueObject instanceof Entity) {
-                parentId = ((Entity) valueObject).getId();
-            }
+	@Action
+	public void detach() throws UnifyException {
+		if (handler == null) {
+			setFilename(null);
+			setValue(null);
+		} else {
+			// Detach
+			Object uploadId = getUploadId();
+			Object parentId = null;
+			Object valueObject = getValueStore().getValueObject();
+			if (valueObject instanceof Entity) {
+				parentId = ((Entity) valueObject).getId();
+			}
 
-            if (uploadId != null) {
-                handler.delete(uploadId, getUplAttribute(String.class, "category"), parentId,
-                        getUplAttribute(String.class, "parentCategory"), getUplAttribute(String.class, "parentFieldName"));
-                setUploadId(null);
-            }
-    	}
-    }
+			if (uploadId != null) {
+				handler.delete(uploadId, getUplAttribute(String.class, "category"), parentId,
+						getUplAttribute(String.class, "parentCategory"),
+						getUplAttribute(String.class, "parentFieldName"));
+				setUploadId(null);
+			}
+		}
+	}
 
-    @Override
-    public void addPageAliases() throws UnifyException {
-        addPageAlias(fileCtrl);
-    }
+	@Override
+	public void addPageAliases() throws UnifyException {
+		addPageAlias(fileCtrl);
+	}
 
-    public String getViewPath() throws UnifyException {
-        return getUplAttribute(String.class, "viewPath");
-    }
+	public String getViewPath() throws UnifyException {
+		return getUplAttribute(String.class, "viewPath");
+	}
 
-    public FileAttachmentType getType() throws UnifyException {
-        return getUplAttribute(FileAttachmentType.class, "type");
-    }
+	public FileAttachmentType getType() throws UnifyException {
+		return getUplAttribute(FileAttachmentType.class, "type");
+	}
 
-    public String getFilenameBinding() throws UnifyException {
-        return getUplAttribute(String.class, "filenameBinding");
-    }
+	public String getFilenameBinding() throws UnifyException {
+		return getUplAttribute(String.class, "filenameBinding");
+	}
 
-    public Object getUploadId() throws UnifyException {
-        return getValue();
-    }
+	public Object getUploadId() throws UnifyException {
+		return getValue();
+	}
 
-    public FileUpload getFileCtrl() {
-        return fileCtrl;
-    }
+	public FileUpload getFileCtrl() {
+		return fileCtrl;
+	}
 
-    public Control getAttachCtrl() {
-        return attachCtrl;
-    }
+	public Control getAttachCtrl() {
+		return attachCtrl;
+	}
 
-    public Control getViewCtrl() {
-        return viewCtrl;
-    }
+	public Control getViewCtrl() {
+		return viewCtrl;
+	}
 
-    public Control getRemoveCtrl() {
-        return removeCtrl;
-    }
+	public Control getRemoveCtrl() {
+		return removeCtrl;
+	}
 
-    @Override
+	@Override
 	public boolean isRefreshesContainer() {
 		return true;
 	}
 
 	@Override
-    protected void doOnPageConstruct() throws UnifyException {
-        FileAttachmentType _type = getType();
-        fileCtrl = (FileUpload) addInternalChildWidget(
-                "!ui-fileupload accept:" + _type + " selectOnly:true hidden:true");
-        attachCtrl = (Control) addInternalChildWidget(
-                "!ui-button styleClass:$e{fabutton} caption:$m{button.attach} hint:$m{button.attach} debounce:false");
-        viewCtrl = (Control) addInternalChildWidget(
-                "!ui-button styleClass:$e{fabutton} caption:$m{button.view} hint:$m{button.view} debounce:false");
-        removeCtrl = (Control) addInternalChildWidget(
-                "!ui-button styleClass:$e{fabutton-alert} caption:$m{button.remove} hint:$m{button.remove} debounce:false");
-        String _handler = getUplAttribute(String.class, "handler");
-        handler = !StringUtils.isBlank(_handler) ?  (FileUploadViewHandler) getComponent(_handler) : null;
-    }
+	protected void doOnPageConstruct() throws UnifyException {
+		FileAttachmentType _type = getType();
+		fileCtrl = (FileUpload) addInternalChildWidget(
+				"!ui-fileupload accept:" + _type + " selectOnly:true hidden:true");
+		attachCtrl = (Control) addInternalChildWidget(
+				"!ui-button styleClass:$e{fabutton} caption:$m{button.attach} hint:$m{button.attach} debounce:false");
+		viewCtrl = (Control) addInternalChildWidget(
+				"!ui-button styleClass:$e{fabutton} caption:$m{button.view} hint:$m{button.view} debounce:false");
+		removeCtrl = (Control) addInternalChildWidget(
+				"!ui-button styleClass:$e{fabutton-alert} caption:$m{button.remove} hint:$m{button.remove} debounce:false");
+		String _handler = getUplAttribute(String.class, "handler");
+		handler = !StringUtils.isBlank(_handler) ? (FileUploadViewHandler) getComponent(_handler) : null;
+	}
 
-    protected void setUploadId(Object uploadId) throws UnifyException {
-        setValue(uploadId);
-    }
-    
-    protected void setFilename(String filename) throws UnifyException {
-    	String filenameBinding = getFilenameBinding();
-    	if (!StringUtils.isBlank(filenameBinding)) {
-    		setValue(filenameBinding, filename);
-    	}
-    }
-    
-    protected String getFilename() throws UnifyException {
-    	String filenameBinding = getFilenameBinding();
-    	if (!StringUtils.isBlank(filenameBinding)) {
-    		return getValue(String.class, filenameBinding);
-    	}
-    	
-    	return null;
-    }
+	protected void setUploadId(Object uploadId) throws UnifyException {
+		setValue(uploadId);
+	}
+
+	protected void setFilename(String filename) throws UnifyException {
+		String filenameBinding = getFilenameBinding();
+		if (!StringUtils.isBlank(filenameBinding)) {
+			setValue(filenameBinding, filename);
+		}
+	}
+
+	protected String getFilename(FileAttachmentType type) throws UnifyException {
+		String filenameBinding = getFilenameBinding();
+		if (!StringUtils.isBlank(filenameBinding)) {
+			String filename = getValue(String.class, filenameBinding);
+			return !StringUtils.isBlank(filename) ? filename : type.appendDefaultExtension("file");
+		}
+
+		return null;
+	}
 }
