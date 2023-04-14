ce681ebbfb57f6582cdec8c4a365ff11f9e12c68
Lateef Ojulari <lateef.ojulari@tcdng.com>
2023-04-14 07:23:20 +0100

    Add default alignment type

--- unify-core/src/main/java/com/tcdng/unify/core/constant/DataType.java
+++ unify-core/src/main/java/com/tcdng/unify/core/constant/DataType.java
@@ -36,73 +36,81 @@ import com.tcdng.unify.core.util.EnumUtils;
 @StaticList(name = "datatypelist", description = "$m{staticlist.datatypelist}")
 public enum DataType implements EnumConst {
 
-    CHAR("CH", Character.class, ColumnType.CHARACTER),
-    BOOLEAN("BL", Boolean.class, ColumnType.BOOLEAN),
-    SHORT("SH", Short.class, ColumnType.SHORT),
-    INTEGER("IN", Integer.class, ColumnType.INTEGER, SHORT),
-    LONG("LN", Long.class, ColumnType.LONG, INTEGER, SHORT),
-    FLOAT("FL", Float.class, ColumnType.FLOAT),
-    DOUBLE("DB", Double.class, ColumnType.DOUBLE, FLOAT),
-    DECIMAL("DC", BigDecimal.class, ColumnType.DECIMAL, DOUBLE, FLOAT, LONG, INTEGER, SHORT),
-    DATE("DT", Date.class, ColumnType.DATE),
-    TIMESTAMP("TS", Date.class, ColumnType.TIMESTAMP, DATE),
-    TIMESTAMP_UTC("TU", Date.class, ColumnType.TIMESTAMP_UTC, DATE, TIMESTAMP),
-    CLOB("CT", String.class, ColumnType.CLOB),
-    BLOB("BT", byte[].class, ColumnType.BLOB),
-    STRING("ST", String.class, ColumnType.STRING, CHAR, BOOLEAN, DECIMAL, DOUBLE, FLOAT, LONG, INTEGER, SHORT, CLOB);
+	CHAR("CH", Character.class, ColumnType.CHARACTER, HAlignType.LEFT),
+	BOOLEAN("BL", Boolean.class, ColumnType.BOOLEAN, HAlignType.CENTER),
+	SHORT("SH", Short.class, ColumnType.SHORT, HAlignType.RIGHT),
+	INTEGER("IN", Integer.class, ColumnType.INTEGER, HAlignType.RIGHT, SHORT),
+	LONG("LN", Long.class, ColumnType.LONG, HAlignType.RIGHT, INTEGER, SHORT),
+	FLOAT("FL", Float.class, ColumnType.FLOAT, HAlignType.RIGHT),
+	DOUBLE("DB", Double.class, ColumnType.DOUBLE, HAlignType.RIGHT, FLOAT),
+	DECIMAL("DC", BigDecimal.class, ColumnType.DECIMAL, HAlignType.RIGHT, DOUBLE, FLOAT, LONG, INTEGER, SHORT),
+	DATE("DT", Date.class, ColumnType.DATE, HAlignType.LEFT),
+	TIMESTAMP("TS", Date.class, ColumnType.TIMESTAMP, HAlignType.LEFT, DATE),
+	TIMESTAMP_UTC("TU", Date.class, ColumnType.TIMESTAMP_UTC, HAlignType.LEFT, DATE, TIMESTAMP),
+	CLOB("CT", String.class, ColumnType.CLOB, HAlignType.CENTER),
+	BLOB("BT", byte[].class, ColumnType.BLOB, HAlignType.CENTER), STRING("ST", String.class, ColumnType.STRING,
+			HAlignType.LEFT, CHAR, BOOLEAN, DECIMAL, DOUBLE, FLOAT, LONG, INTEGER, SHORT, CLOB);
 
-    private final String code;
+	private final String code;
 
-    private final Class<?> javaClass;
+	private final Class<?> javaClass;
 
-    private final ColumnType columnType;
+	private final ColumnType columnType;
 
-    private final Set<DataType> convertibleFromTypes;
+	private final HAlignType alignType;
 
-    private DataType(String code, Class<?> javaClass, ColumnType columnType, DataType... convertibleFromTypes) {
-        this.code = code;
-        this.javaClass = javaClass;
-        this.columnType = columnType;
-        Set<DataType> set = new HashSet<DataType>();
-        set.add(this);
-        if (convertibleFromTypes != null && convertibleFromTypes.length > 0) {
-            set.addAll(Arrays.asList(convertibleFromTypes));
-        }
+	private final Set<DataType> convertibleFromTypes;
 
-        this.convertibleFromTypes = Collections.unmodifiableSet(set);
-    }
+	private DataType(String code, Class<?> javaClass, ColumnType columnType, HAlignType alignType,
+			DataType... convertibleFromTypes) {
+		this.code = code;
+		this.javaClass = javaClass;
+		this.columnType = columnType;
+		this.alignType = alignType;
+		Set<DataType> set = new HashSet<DataType>();
+		set.add(this);
+		if (convertibleFromTypes != null && convertibleFromTypes.length > 0) {
+			set.addAll(Arrays.asList(convertibleFromTypes));
+		}
 
-    @Override
-    public String code() {
-        return this.code;
-    }
+		this.convertibleFromTypes = Collections.unmodifiableSet(set);
+	}
 
-    @Override
-    public String defaultCode() {
-        return STRING.code;
-    }
+	@Override
+	public String code() {
+		return this.code;
+	}
 
-    public Class<?> javaClass() {
-        return javaClass;
-    }
+	@Override
+	public String defaultCode() {
+		return STRING.code;
+	}
 
-    public ColumnType columnType() {
-        return columnType;
-    }
+	public Class<?> javaClass() {
+		return javaClass;
+	}
 
-    public Set<DataType> convertibleFromTypes() {
-        return convertibleFromTypes;
-    }
+	public ColumnType columnType() {
+		return columnType;
+	}
 
-    public boolean isConvertibleFrom(DataType dataType) {
-        return this.equals(dataType) || convertibleFromTypes.contains(dataType);
-    }
+	public HAlignType alignType() {
+		return alignType;
+	}
 
-    public static DataType fromCode(String code) {
-        return EnumUtils.fromCode(DataType.class, code);
-    }
+	public Set<DataType> convertibleFromTypes() {
+		return convertibleFromTypes;
+	}
 
-    public static DataType fromName(String name) {
-        return EnumUtils.fromName(DataType.class, name);
-    }
+	public boolean isConvertibleFrom(DataType dataType) {
+		return this.equals(dataType) || convertibleFromTypes.contains(dataType);
+	}
+
+	public static DataType fromCode(String code) {
+		return EnumUtils.fromCode(DataType.class, code);
+	}
+
+	public static DataType fromName(String name) {
+		return EnumUtils.fromName(DataType.class, name);
+	}
 }
