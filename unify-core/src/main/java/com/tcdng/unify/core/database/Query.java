fdf9340bfcb461b209a4cbd48f2c0b60be56c5d2
Lateef Ojulari <lateef.ojulari@tcdng.com>
2025-01-09 11:54:06 +0100

    Extend query object

--- unify-core/src/main/java/com/tcdng/unify/core/database/Query.java
+++ unify-core/src/main/java/com/tcdng/unify/core/database/Query.java
@@ -381,6 +381,13 @@ public class Query<T extends Entity> implements Cloneable {
         return this;
     }
 
+    public Query<T> addSelect(Collection<String> fields) {
+        for (String field : fields) {
+            innerGetSelect().add(field);
+        }
+        return this;
+    }
+
     public Query<T> addSelect(Select select) {
         innerGetSelect().addAll(select.values());
         return this;
@@ -388,6 +395,13 @@ public class Query<T extends Entity> implements Cloneable {
     
     public Query<T> addGroupBy(String field) {
         innerGetGroupBy().add(field);
+        return this;
+    }
+
+    public Query<T> addGroupBy(Collection<String> fields) {
+        for (String field : fields) {
+            innerGetGroupBy().add(field);
+        }
         return this;
     }
 
@@ -400,6 +414,13 @@ public class Query<T extends Entity> implements Cloneable {
 
     public Query<T> addOrder(String field) {
         getOrder().add(field);
+        return this;
+    }
+
+    public Query<T> addOrder(Collection<String> fields) {
+        for (String field : fields) {
+            getOrder().add(field);
+        }
         return this;
     }
 
