6a0dc5f1cb00819f91f437670d39338007f2fb12
Lateef Ojulari <lateef.ojulari@tcdng.com>
2025-01-10 00:07:04 +0100

    Add toString

--- unify-core/src/main/java/com/tcdng/unify/core/data/UploadedFile.java
+++ unify-core/src/main/java/com/tcdng/unify/core/data/UploadedFile.java
@@ -64,4 +64,10 @@ public class UploadedFile {
         return 0;
     }
 
+	@Override
+	public String toString() {
+		return "UploadedFile [filename=" + filename + ", creationDate=" + creationDate + ", modificationDate="
+				+ modificationDate + ", data=" + (data != null ? data.length : 0) + "]";
+	}
+
 }
