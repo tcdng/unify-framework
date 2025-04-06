fefbc3695fd950a793996c6a4eca6fc8370a977d
Lateef Ojulari <lateef.ojulari@tcdng.com>
2025-04-06 23:09:14 +0100

    Redefine scripting engine

--- unify-core/src/main/java/com/tcdng/unify/core/script/AbstractScriptingEngine.java
+++ unify-core/src/main/java/com/tcdng/unify/core/script/AbstractScriptingEngine.java
@@ -17,15 +17,9 @@
 package com.tcdng.unify.core.script;
 
 import java.util.Collections;
-import java.util.Map;
-
-import javax.script.Bindings;
-import javax.script.ScriptEngine;
-import javax.script.ScriptException;
 
 import com.tcdng.unify.core.AbstractUnifyComponent;
 import com.tcdng.unify.core.UnifyException;
-import com.tcdng.unify.core.util.DataUtils;
 
 /**
  * Convenient abstract base class for scripting engines.
@@ -34,44 +28,19 @@ import com.tcdng.unify.core.util.DataUtils;
  * @since 1.0
  */
 public abstract class AbstractScriptingEngine extends AbstractUnifyComponent implements ScriptingEngine {
-
-    private ScriptEngine scriptEngine;
     
     @Override
-    public <T> T evaluate(Class<T> resultType, String script) throws UnifyException {
+    public <T extends Number> T evaluate(Class<T> resultType, String script) throws UnifyException {
         return evaluate(resultType, script, Collections.emptyMap());
     }
 
     @Override
-    public <T> T evaluate(Class<T> resultType, String script, Map<String, Object> params) throws UnifyException {
-        Object result = null;
-        try {
-            if (params == null ||  params.isEmpty()) {
-                result = scriptEngine.eval(script);
-            } else {
-                Bindings bindings = scriptEngine.createBindings();
-                for (Map.Entry<String, Object> entry: params.entrySet()) {
-                    bindings.put(entry.getKey(), entry.getValue());
-                }
-                
-                result = scriptEngine.eval(script, bindings);
-            }
-        } catch (ScriptException e) {
-            throwOperationErrorException(e);
-        }
-
-        return DataUtils.convert(resultType, result);
-    }
-
-    @Override
     protected void onInitialize() throws UnifyException {
-        scriptEngine = createScriptEngine();
+
     }
 
     @Override
     protected void onTerminate() throws UnifyException {
 
     }
-
-    protected abstract ScriptEngine createScriptEngine() throws UnifyException;
 }
